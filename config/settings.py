f3e28eba3728b75f2159d32f73b0b71a936a82b3
Emmir-1 <emmir.se@gmail.com>
2023-07-12 18:56:52 +0600

    12

--- config/settings.py
+++ config/settings.py
@@ -62,8 +62,6 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
