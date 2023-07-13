6292b456243f5e503dd144ee6b23528cf697c5c9
valla <kochemarov@gmail.com>
2023-07-13 21:17:03 +0600

    add parent to detail review product

--- category/models.py
+++ category/models.py
@@ -7,7 +7,8 @@ from django.template.defaultfilters import slugify as django_slugify
 class Category(models.Model):
     slug = models.SlugField(max_length=50, primary_key=True)
     name = models.CharField(max_length=50, unique=True)
-    parent = models.ForeignKey('self', on_delete=models.SET_NULL, related_name='children', blank=True, null=True)
+    parent = models.ForeignKey('self', on_delete=models.SET_NULL,
+                               related_name='children', blank=True, null=True)
 
     def __str__(self):
         return f'{self.parent} -> {self.name}' if self.parent else f'{self.name}'
