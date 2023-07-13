88cdbb62816ad8cac5801cf4a0fa15324024196c
valla <kochemarov@gmail.com>
2023-07-13 14:17:21 +0600

    likes and favorites done by Emmir and write by toggle

--- product/views.py
+++ product/views.py
@@ -1,10 +1,14 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import viewsets, permissions
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import viewsets, permissions, status
+from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 from . import serializers
-from .models import Product, ProductImage
+from .models import Product, ProductImage, Likes, Favorite
 from .permissions import IsAuthorOrAdmin, IsAuthor
 from .serializers import ProductSerializer
 
@@ -40,9 +44,46 @@ class ProductViewSet(viewsets.ModelViewSet):
         return serializers.ProductSerializer
 
     def get_permissions(self):
-        if self.action == 'retrieve' or self.action == 'list':
-            return [IsAuthor(), ]
+        if self.action in ('retrieve', 'list', 'toggle_like', 'toggle_favorites'):
+            return [permissions.AllowAny(), ]
         return [permissions.IsAdminUser(), ]
+
+    @action(detail=True, methods=['GET'])
+    def toggle_like(self, request, pk):
+        product = self.get_object()
+        user = request.user
+        like_obj, created = Likes.objects.get_or_create(product=product, user=user)
+
+        like_obj.is_liked = not like_obj.is_liked
+        like_obj.save()
+        return Response('like toggled')
+
+    # api/v1/products/id/toggle_favorites/
+    @action(detail=True, methods=['GET'])
+    def toggle_favorites(self, request, pk):
+        product = self.get_object()
+        user = request.user
+        fav, created = Favorite.objects.get_or_create(product=product, user=user)
+
+        fav.favorite = not fav.favorite
+        fav.save()
+        return Response('favourite toggled')
+
+    @swagger_auto_schema(manual_parameters=[
+        openapi.Parameter('likes_from', openapi.IN_QUERY, 'filter products by amount of likes', True,
+                          type=openapi.TYPE_INTEGER)])
+    @action(detail=False, methods=["GET"])
+    def likes(self, request, pk=None):
+        from django.db.models import Count
+        q = request.query_params.get("likes_from")  # request.query_params = request.GET
+        queryset = self.get_queryset()
+        queryset = queryset.annotate(Count('likes')).filter(likes__count__gte=q)
+
+        serializer = ProductSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def get_serializer_context(self):
+        return {'request': self.request}
 
     # api/v1/products/<id>/reviews/
     # @action(['GET', 'POST'], detail=True)
