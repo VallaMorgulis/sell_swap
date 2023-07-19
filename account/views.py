0b77673320256f9a45af844c04badb51f6b2efe6
valla <kochemarov@gmail.com>
2023-07-19 14:14:31 +0600

    get permission to refresh a password

--- account/views.py
+++ account/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from django.urls import path
 from django.views.decorators.cache import cache_page
 from rest_framework.decorators import action
-from rest_framework.permissions import AllowAny, SAFE_METHODS
+from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.mixins import ListModelMixin, UpdateModelMixin
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
@@ -15,24 +15,20 @@ from django.contrib.auth.models import User
 from product.permissions import IsAuthor
 from product.serializers import FavoriteListSerializer
 from .serializers import ChangePasswordSerializer
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 
 from account import serializers
 from account.send_mail import send_confirmation_email
-
 # from favorite.serializers import FavoriteUserSerializer
 
 User = get_user_model()
 
 
-class UserViewSet(ListModelMixin, UpdateModelMixin, GenericViewSet):
+class UserViewSet(ListModelMixin, GenericViewSet):
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
+    permission_classes = (AllowAny,)
 
-    def get_permissions(self):
-        if self.action in ('update', 'partial_update'):  # PUT и PATCH
-            return (IsAuthor(), )
-        return (AllowAny(),)
 
     @action(['POST'], detail=False)
     def register(self, request, *args, **kwargs):
